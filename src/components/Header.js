a2ab47cb9bdaf5d5972e7fe03c275f50728eff1a
cjwood032 <cjwood032@gmail.com>
2019-01-30 09:24:21 -0500

    adding bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,25 @@
 import React from 'react';
+
 import { Link } from 'react-router-dom';
 
    const Header = () => (
      <header>
       <div className="nav">
         <div className="nav-heading">
-        <div className="nav-title">
-            Career Tracker
-        </div>
-        </div>
-        <div className="nav-links">
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/About"> About </Link></li>
-          <li><Link to="/Careers"> Careers </Link></li>
-          <li><Link to="/Details"> Contacts</Link></li>
-          
-        </ul>
-        </div>
+          <div className="nav-title">
+          </div>
+          </div>
+          <nav class="navbar navbar-expand-lg navbar-light">
+          <h3 class ="navbar-brand">Career Tracker</h3>
+           <div class="collapse navbar-collapse" id="nav-links">
+            <ul class="navbar-nav">
+              <li class="nav-item"><Link class="nav-link" to="/">Home</Link></li>
+              <li class="nav-item"><Link class="nav-link" to="/About">About</Link></li>
+              <li class="nav-item"><Link class="nav-link" to="/Careers">Careers</Link></li>
+              <li class="nav-item"><Link class="nav-link" to="/Details">Contacts</Link></li>
+            </ul>
+            </div>
+          </nav>
       </div>
      </header>
    )
