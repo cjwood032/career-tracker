6b488a6d148f39624ef495d36df82e0c8966ea90
Christopher Wood <Cjwood032@gmail.com>
2019-03-31 02:03:23 -0400

    Detail updated with new data

--- src/components/details/Detail.js
+++ src/components/details/Detail.js
@@ -5,18 +5,24 @@ class Detail extends Component {
     this.props.deleteDetail(this.props.detail.id)
   }
   render() { 
-
+	//if complete have the card faded out
     return (
       <div className="detailcard">
+				<h4> {this.props.detail.status}</h4>
         <button onClick={this.handleClick}>Delete</button><br/>
-        Company: <b>{this.props.detail.company}</b><br/>
+        Link: <a href={this.props.detail.jobLink}>{this.props.detail.jobLink}</a>
+				Title: <strong>{this.props.detail.jobTitle}</strong>
+				Company: <b>{this.props.detail.company}</b><br/>
+				Referral: <b>{this.props.detail.referral}</b>
         Date of last update:<b>{ this.props.detail.updated}</b><br/>
         Date of last contact: <b>{this.props.detail.contacted}</b><br/>
+				Contact method: {this.props.detail.method}
         Contact Name: <b>{this.props.detail.name}</b><br/>
         Contact Email: <b>{this.props.detail.email}</b><br/>
         Contact Title: <b>{this.props.detail.role}</b>
-        
-        
+				Contacted via:<p>{this.props.detail.action}</p>
+				Next step: {this.props.detail.step}
+				<p>notes</p>  
       </div>
     );
   }
