6d3b290db830b247223293bdbe1543d5600e4ea4
Christopher Wood <Cjwood032@gmail.com>
2019-08-29 08:21:35 -0400

    Interview form update inputs

--- src/components/interviews/InterviewInput.js
+++ src/components/interviews/InterviewInput.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
-import {createCareer} from '../../actions/interviewActions'
+import {createInterview} from '../../actions/interviewActions'
 import {updateFormData} from '../../actions/formActions'
 import "./InterviewInput.css"
-class CareerInput extends Component {
+class InterviewInput extends Component {
 
 
   handleChange = event => {
@@ -17,17 +17,20 @@ class CareerInput extends Component {
     this.props.createInterview(this.props.formData)
   }
   render() {
-    const {company, about_company, name, job_title, link, date, job_description, notes,} = this.props.formData
+    const {company, name, date, job_title, requirements, job_description, notes, reviews, about_company} = this.props.formData
     return (
       <div>
-        <h2>Add an Opening:</h2>
-        <form className="CareerForm" onSubmit={this.handleSubmit}>
-          Title<input type="text" name="title" value={title} onChange={this.handleChange}/>
+        <h2>Plan an Interview:</h2>
+        <form className="InterviewForm" onSubmit={this.handleSubmit}>
+          Title<input type="text" name="name" value={name} onChange={this.handleChange}/>
           Company<input type="text" name="company" value={company} onChange={this.handleChange}/>
-          URL<input type="url" name="link" value={link} onChange={this.handleChange}/>
-          Location<input type="text" name="location" value={location} onChange={this.handleChange}/><br/>
-          Description<textarea name="description" value={description} onChange={this.handleChange} />
-          
+          Appointment time<input type="datetime-local" name="date" value={date} onChange={this.handleChange}/>
+          Job Title<input type="text" name="job_title" value={job_title} onChange={this.handleChange}/><br/>
+          Requirements<textarea name="requirements" value={requirements} onChange={this.handleChange} />
+          Description<textarea name="job_description" value={job_description} onChange={this.handleChange} />
+          Notes<textarea name="notes" value={notes} onChange={this.handleChange} />
+          About Company<textarea name="about_company" value={about_company} onChange={this.handleChange} />
+          Reviews<textarea name="reviews" value={reviews} onChange={this.handleChange} />
           <input type="submit" />
         </form>
       </div>
@@ -40,4 +43,4 @@ const mapStateToProps = state => {
   })
 }
 
-export default connect(mapStateToProps, {createCareer, updateFormData})(CareerInput);
+export default connect(mapStateToProps, {createInterview, updateFormData})(InterviewInput);
