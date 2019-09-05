93aa560ac92d7d3c4e588f90b7d5242242c9011c
Christopher Wood <Cjwood032@gmail.com>
2019-09-04 20:49:40 -0400

    Interview.js update, planning component

---
+++ src/components/interviews/Interview.js
@@ -0,0 +1,8 @@
+//company, name, date, job_title, requirements, job_description, notes, reviews, about_company
+// [larger title]Date
+// [small titles] name, company, job_title
+//[text] about, description
+// [List]requirements
+// [list?]notes
+// need to be able to enter notes, person's of interest? 
+// need to be able to add additional notes.
